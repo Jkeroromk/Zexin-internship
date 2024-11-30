0f618a92611e219f8fe5cfcee71dd7eca6c7adf3
Jkeroromk <zzou2000@gmail.com>
2024-11-30 14:27:51 -0500

    starting on explore item section

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const ExploreItems = () => {
+  const [explore, setExplore] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const Explore = async () => {
+      try {
+        const res = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/explore"
+        );
+        setExplore(res.data);
+        console.log(res.data);
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching new items:", err);
+        setLoading(false);
+      }
+    };
+
+    Explore();
+  }, []);
+
   return (
     <>
       <div>
@@ -14,7 +36,7 @@ const ExploreItems = () => {
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
+      {explore.map((items, index) => (
         <div
           key={index}
           className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
@@ -27,7 +49,7 @@ const ExploreItems = () => {
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img className="lazy" src={items.authorImage} alt="" />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
@@ -52,17 +74,17 @@ const ExploreItems = () => {
                 </div>
               </div>
               <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+                <img src={items.nftImage} className="lazy nft__item_preview" alt="" />
               </Link>
             </div>
             <div className="nft__item_info">
               <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+                <h4>{items.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
+              <div className="nft__item_price">{items.price} ETH</div>
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{items.likes}</span>
               </div>
             </div>
           </div>
