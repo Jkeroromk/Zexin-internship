4e4362a1b6dcd8e426574f7bcc64baee97074380
Jason Zou <jasonzou@JasondeMacBook-Air.local>
2024-12-02 10:58:17 -0500

    finished the Author-section by adding the copy and paste promp and the skeleton loading stage

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
-import AuthorBanner from "../images/author_banner.jpg";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
 
 const Author = () => {
-  
   const { authorId } = useParams();
   const [authorData, setAuthorData] = useState();
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
+  const [isFollowing, setIsFollowing] = useState(false); // Tracks follow/unfollow state
 
   useEffect(() => {
     const fetchAuthorData = async () => {
@@ -18,7 +18,6 @@ const Author = () => {
           `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorId}`
         );
         setAuthorData(response.data);
-        console.log(response.data);
       } catch (err) {
         console.error("Error fetching author data:", err);
       } finally {
@@ -29,13 +28,28 @@ const Author = () => {
     fetchAuthorData();
   }, [authorId]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handleCopy = () => {
+    if (authorData?.address) {
+      navigator.clipboard.writeText(authorData.address).then(() => {
+        alert("Address copied to clipboard!");
+      });
+    }
+  };
 
-  if (!authorData) {
-    return <div>Author not found.</div>;
-  }
+  const handleFollowToggle = () => {
+    if (authorData) {
+      const updatedFollowers = isFollowing
+        ? authorData.followers - 1
+        : authorData.followers + 1;
+
+      setAuthorData((prevData) => ({
+        ...prevData,
+        followers: updatedFollowers,
+      }));
+
+      setIsFollowing((prev) => !prev);
+    }
+  };
 
   return (
     <div id="wrapper">
@@ -47,8 +61,14 @@ const Author = () => {
           aria-label="section"
           className="text-light"
           data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${authorData.authorImage}) top` }}
-        ></section>
+          style={{
+            background: loading
+              ? undefined
+              : `url(${authorData?.authorImage}) top`,
+          }}
+        >
+          {loading && <Skeleton height={300} />}
+        </section>
 
         <section aria-label="section">
           <div className="container">
@@ -57,29 +77,60 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={authorData.authorImage} alt="" />
-
+                      {loading ? (
+                        <Skeleton circle={true} height={100} width={100} />
+                      ) : (
+                        <img src={authorData.authorImage} alt="" />
+                      )}
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          {authorData.authorName}
-                          <span className="profile_username">@{authorData.tag}</span>
-                          <span id="wallet" className="profile_wallet">
-                            {authorData.address}
+                          {loading ? (
+                            <Skeleton width={200} />
+                          ) : (
+                            authorData.authorName
+                          )}
+                          <span className="profile_username">
+                            {loading ? (
+                              <Skeleton width={100} />
+                            ) : (
+                              `@${authorData.tag}`
+                            )}
                           </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
+                          <span id="wallet" className="profile_wallet">
+                            {loading ? (
+                              <Skeleton width={250} />
+                            ) : (
+                              authorData.address
+                            )}
+                          </span>
+                          {!loading && (
+                            <button
+                              id="btn_copy"
+                              title="Copy Text"
+                              onClick={handleCopy}
+                            >
+                              Copy
+                            </button>
+                          )}
                         </h4>
                       </div>
                     </div>
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">{authorData.followers} followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
+                      <div className="profile_follower">
+                        {loading ? (
+                          <Skeleton width={50} />
+                        ) : (
+                          `${authorData.followers} followers`
+                        )}
+                      </div>
+                      {!loading && (
+                        <button className="btn-main" onClick={handleFollowToggle}>
+                          {isFollowing ? "Unfollow" : "Follow"}
+                        </button>
+                      )}
                     </div>
                   </div>
                 </div>
@@ -87,7 +138,14 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                <AuthorItems authorId={authorId} />
+                  {loading ? (
+                    <Skeleton height={200} />
+                  ) : (
+                    <AuthorItems
+                      authorId={authorId}
+                      authorImage={authorData.authorImage}
+                    />
+                  )}
                 </div>
               </div>
             </div>
